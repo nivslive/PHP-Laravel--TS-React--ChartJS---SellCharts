ba9e478893837ec58ab8967eef258f8b7086dec3
nivs <nivsoficial@gmail.com>
2023-09-03 23:02:41 -0300

    fix: [BACKEND] atts about 2020 seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -63,6 +63,44 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
+        // 2020 SELLS
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-03-31 22:00:58',
+        ]);
+
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-05-31 22:00:58',
+        ]);
+
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-02-31 22:00:58',
+        ]);
+
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-06-31 22:00:58',
+        ]);
+
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-10-31 22:00:58',
+        ]);
+
+        \App\Models\Sell::create([
+            'product_id' => $product->id,
+            'price' => 250.50,
+            'created_at' => '2020-12-31 22:00:58',
+        ]);
+
+
         // SELLS FORECASTS
         \App\Models\SellForecast::create([
             'price' => 250.50,
